592ddf44c766aff7d0f7e047031cdd5f460f4f0b
gegarvaganza <ahmadhakim305@gmail.com>
2024-12-27 16:38:51 +0800

    THE MAZE (REAL) BEFORE NAVMESH UPDATE PART 3

--- Assets/PatrollState.cs
+++ Assets/PatrollState.cs
@@ -7,9 +7,14 @@ public class PatrollState : StateMachineBehaviour
     float timer;
     List<Transform> wayPoints = new List<Transform>();
     NavMeshAgent agent;
+
+    Transform player;
+    float chaseRange = 8;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = animator.GetComponent<NavMeshAgent>();
         timer = 0;
         GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
@@ -28,6 +33,10 @@ public class PatrollState : StateMachineBehaviour
         timer += Time.deltaTime;
         if (timer > 10)
             animator.SetBool("IsPatrolling", false);
+
+        float distance = Vector3.Distance(player.transform.position, animator.transform.position);
+        if (distance < chaseRange)
+            animator.SetBool("IsChasing", true);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state

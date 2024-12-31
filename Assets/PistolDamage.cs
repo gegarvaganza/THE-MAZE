ee0f126603598eaf6f65f6bc98dc0c272157a089
gegarvaganza <ahmadhakim305@gmail.com>
2025-01-01 00:46:47 +0800

    THE MAZE (REAL) UPDATE PART 8

---
+++ Assets/PistolDamage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolDamage : MonoBehaviour
+{
+    public int damageAmount = 20;
+    private void Start()
+    {
+        Destroy(gameObject, 10);  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(transform.GetComponent<Rigidbody>());
+        if(other.tag == "IceMonster")
+        {
+            transform.parent = other.transform;
+            other.GetComponent<IceMonster>().TakeDamage(damageAmount);
+        }
+    }
+}

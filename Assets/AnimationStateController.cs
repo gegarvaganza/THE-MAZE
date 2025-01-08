662440bf7cf623752305dda2ed76a2e92a1da080
gegarvaganza <ahmadhakim305@gmail.com>
2025-01-08 21:00:48 +0800

    THE MAZE (REAL) UPDATE PART 14

--- Assets/AnimationStateController.cs
+++ Assets/AnimationStateController.cs
@@ -7,19 +7,24 @@ public class AnimationStateController : MonoBehaviour
 {
     Animator animator;
     int IsWalkingHash;
+    int IsRunningHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         IsWalkingHash = Animator.StringToHash("IsWalking");
+        IsRunningHash = Animator.StringToHash("IsRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool IsRunning = animator.GetBool(IsRunningHash);
         bool IsWalking = animator.GetBool(IsWalkingHash);
         bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
         // if player press w key
         if (!IsWalking && forwardPressed)
         {
@@ -34,6 +39,19 @@ public class AnimationStateController : MonoBehaviour
             animator.SetBool(IsWalkingHash, false);
         }
 
+        // if player is walking and not running and presses left shift
+        if (!IsRunning && (forwardPressed && runPressed))
+        {
+            // then set the IsRunning boolean to be true
+            animator.SetBool(IsRunningHash, true);
+        }
+
+        // if player is running and stops running or stops walking
+        if (IsRunning && (!forwardPressed || !runPressed))
+        {
+            // then set the IsRunning boolean to be false
+            animator.SetBool(IsRunningHash, false);
+        }
 
     }
 }
